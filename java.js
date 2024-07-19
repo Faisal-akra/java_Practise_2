b2026eb57429a9ed0ac8e2ad4361eb3ebeca6721
Faisal-Akram <zjan45097@gmail.com>
2024-07-19 22:16:33 +0500

    done all

---
+++ java.js
@@ -0,0 +1,62 @@
+//(6) Find the smallest Number:
+
+// var array =  [];
+// var max = 100;
+
+// for (var i = 1; i <= 5; i++) {
+// var userInput = +prompt("Plese enter 5 numbers");
+//     array.push(userInput)
+
+//     if (userInput < max) {
+//         max = userInput
+//     }
+// }
+// console.log(array);
+// console.log(max);
+
+
+//(7) Convert Celcius to farenheit:
+
+// var userInput = +prompt("Please enter temperatue of C");
+
+// var farenheit = (userInput * 9/5)+32;
+// console.log(farenheit);
+
+
+//(8) Convert farenheit to celcius:
+
+// var userInput = +prompt("enter your tempertaure in F")
+// var celsius = (userInput - 32) * 5 / 9;
+// console.log(celsius);
+
+
+
+
+
+//(9) Reverse An array:
+
+// var userInput = prompt("please enter a messeage");
+// var reverse = "  ";
+
+//    for (var i = userInput.length -1; i >= 0; i--) {
+//     reverse += userInput[i];
+// }
+// console.log(userInput);
+// console.log({reverse});
+
+
+// (10) Calculate average of an array:
+
+// var array = [];
+// var sum = 0;
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("Please enter numbers");
+//     array.push(userInput)
+//     sum += array[i]
+
+// }
+// console.log(array);
+// console.log(sum);
+
+
+
